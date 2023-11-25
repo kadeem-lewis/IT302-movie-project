b8bd625a4196f76e67e13c243f7f2495756f0838
kadeem-lewis <email@kadeemlewis.com>
2023-11-24 22:56:43 -0500

    added tailwind and postcss config files to eslint ignore

--- apps/frontend/.eslintrc.cjs
+++ apps/frontend/.eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  ignorePatterns: ["dist", ".eslintrc.cjs","tailwind.config.ts","postcss.config.cjs"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
