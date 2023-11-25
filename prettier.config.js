31b5cba6a97143cc62b9859acfe69f25e371145a
kadeem-lewis <email@kadeemlewis.com>
2023-11-25 06:20:05 -0500

    updated prettier rules

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,13 @@
 /** @type {import("prettier").Config} */
 const config = {
-
+    singleQuote: false,
+    jsxSingleQuote: false,
+    bracketSpacing: true,
+    trailingComma: "es5",
+    tabWidth: 2,
+    printWidth: 80,
+    semi: true,
+    proseWrap:"preserve"
 };
 
 export default config;
