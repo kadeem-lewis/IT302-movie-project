fe597aa94287be2cfd33746d35773528539712c1
kadeem-lewis <email@kadeemlewis.com>
2023-12-01 15:09:08 -0500

    updated navbar styling and added theme toggle

--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -4,7 +4,6 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Button,
   Avatar,
   Dropdown,
   DropdownItem,
@@ -12,6 +11,7 @@ import {
   DropdownTrigger,
 } from "@nextui-org/react";
 import { User } from "../layouts/RootLayout";
+import ThemeToggle from "./ThemeToggle";
 
 type NavbarProps = {
   user: User | null;
@@ -21,44 +21,40 @@ export default function Navbar({ user }: NavbarProps) {
   return (
     <NextUINavbar className="mb-4" maxWidth="xl">
       <NavbarContent justify="start">
-        <NavbarBrand className="font-bold uppercase">
-          <Link to="/">Movie Reviews</Link>
+        <NavbarBrand as="li" className=" grow-0">
+          <Link to="/" className="gap-2 flex font-bold uppercase items-center">
+            <span className="border rounded-lg bg-black px-1 py-0.5 text-white">
+              MV
+            </span>
+            <span>Movie Reviews</span>
+          </Link>
         </NavbarBrand>
-        <ul className="flex gap-4 justify-start items-center">
-          <NavbarItem>
-            <NavLink to={"/movies"}>Movies</NavLink>
-          </NavbarItem>
-          <NavbarItem>
-            <NavLink to={"/reviews"}>Reviews</NavLink>
-          </NavbarItem>
-        </ul>
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem>
-          {user ? (
-            <>
-              <Dropdown>
-                <DropdownTrigger>
-                  <Avatar name={user.username} size="sm" />
-                </DropdownTrigger>
-                <DropdownMenu>
-                  <DropdownItem>
-                    <NavLink to="">Logout User</NavLink>
-                  </DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-            </>
-          ) : (
-            <div className="flex gap-4">
-              <Button as={Link} to={"/login"} color="primary" variant="flat">
-                Login
-              </Button>
-              <Button as={Link} to={"/signup"} color="primary" variant="flat">
-                Create Account
-              </Button>
-            </div>
-          )}
+          <ThemeToggle />
         </NavbarItem>
+        <Dropdown>
+          <DropdownTrigger>
+            <Avatar name={user?.name} size="sm" />
+          </DropdownTrigger>
+          {user ? (
+            <DropdownMenu>
+              <DropdownItem>
+                <NavLink to="">Logout User</NavLink>
+              </DropdownItem>
+            </DropdownMenu>
+          ) : (
+            <DropdownMenu>
+              <DropdownItem key="login" href="/login">
+                Login
+              </DropdownItem>
+              <DropdownItem key="signup" href="/signup">
+                Signup
+              </DropdownItem>
+            </DropdownMenu>
+          )}
+        </Dropdown>
       </NavbarContent>
     </NextUINavbar>
   );
