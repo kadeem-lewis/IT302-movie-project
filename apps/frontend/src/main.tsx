443b53a93e8a19611c6e5c2702304f79f69fad53
kadeem-lewis <email@kadeemlewis.com>
2023-12-01 15:06:38 -0500

    wrapped app in theme provider

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./styles/index.css";
-import {NextUIProvider} from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider } from "./components/ThemeProvider.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
     </NextUIProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
