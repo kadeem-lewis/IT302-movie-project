54deb7e5c343b2655b19f98764043db29b6fd641
kadeem-lewis <email@kadeemlewis.com>
2024-01-01 23:48:47 -0500

    began creating signup page

--- apps/frontend/src/routes/signup.tsx
+++ apps/frontend/src/routes/signup.tsx
@@ -1,6 +1,49 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Input,
+  Button,
+} from "@nextui-org/react";
+import { Form, Link } from "react-router-dom";
+import { AuthProviders } from "../components/AuthProviders";
 
 export default function Signup() {
   return (
-    <div>signup</div>
-  )
+    <div className="container mx-auto">
+      <Card>
+        <CardHeader>Signup</CardHeader>
+        <CardBody>
+          <Form className="space-y-3">
+            <Input
+              type="text"
+              label="Email"
+              placeholder="Enter email"
+              name="email"
+            />
+            <Input
+              type="text"
+              label="Username"
+              placeholder="Enter username"
+              name="username"
+            />
+            <Input
+              type="password"
+              label="Password"
+              placeholder="Enter password"
+              name="password"
+            />
+            <Button type="submit">Submit</Button>
+          </Form>
+        </CardBody>
+        <CardFooter className="flex flex-col gap-4">
+          <p>
+            Already have an account? <Link to="/login">Login</Link>
+          </p>
+          <AuthProviders />
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
