d287956a4c6edac25ba6178aab1cac84cdd87bdb
kadeem-lewis <email@kadeemlewis.com>
2024-02-02 15:02:42 -0500

    deconstructed mongoclient from mongodb

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,19 +1,18 @@
-import mongodb from "mongodb";
+import { MongoClient } from "mongodb";
 import app from "./server.js";
 import MoviesDAO from "./dao/moviesDAO.js";
-import ReviewsDAO from './dao/reviewsDAO.js'
+import ReviewsDAO from "./dao/reviewsDAO.js";
 import "dotenv/config";
 
 async function main() {
-
-  const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI as string);
+  const client = new MongoClient(process.env.MOVIEREVIEWS_DB_URI as string);
 
   const port = process.env.PORT || 8000;
 
   try {
     await client.connect();
     await MoviesDAO.injectDB(client);
-    await ReviewsDAO.injectDB(client)
+    await ReviewsDAO.injectDB(client);
     app.listen(port, () => {
       console.log("server is running on port:" + port);
     });
